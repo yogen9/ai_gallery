37916bd843d59f7b0d889ba9abdacf0763417280
Yogen Prajapati <yogenprajapati967@gmail.com>
2018-03-13 23:44:04 +0530

    yogen - ajax ready

--- public/js/script.js
+++ public/js/script.js
@@ -10,13 +10,15 @@ function searchEvent(e) {
                 var returnedObject = JSON.parse(this.responseText);
                 console.log(returnedObject);
                 document.getElementById("pics").innerHTML = "";
-                returnedObject.ImageKeys.forEach((image) => {
+                returnedObject.ImageKeys.forEach((image,ind) => {
                     console.log(image);
-                    document.getElementById("pics").innerHTML += "<div class='img-container'> <div  style='background-image:url('https:\\\\s3.us-east-2.amazonaws.com\\yogen1\\" + image + "')"+" class='img' ></div>";
+                    document.getElementById("pics").innerHTML += "<div class='img-container'> <div class='img' id='img"+ind+"'></div>";
+                    document.getElementById("img"+ind).style.backgroundImage = "url('https://s3.us-east-2.amazonaws.com/yogen1/"+image+"')";
                 });
             }
         };
         xhttp.open("GET", "http://localhost:4000/search/" + searchString, true);
         xhttp.send();
+    
     }
 }
